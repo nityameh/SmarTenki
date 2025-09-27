4f176f181b0459a4221bec8a3d100b143b602b90
nityameh <mehtanitya01@gmail.com>
2025-09-27 08:03:32 +0530

    added main * path

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const path=require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
+const __dirname = path.resolve();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 
 // Import services
 const weatherService = require('./services/weatherService');
@@ -257,6 +262,14 @@ app.get('/api/weather/complete/:city', async (req, res) => {
     });
   }
 });
+
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+});
+
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
